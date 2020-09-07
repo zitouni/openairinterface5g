zitouni/openairinterface5g

commit da3dd6b8b0be87d56cd6f5a60f6b622d8544bbf6

benetel: driver for Benetel's 4G and 5G RRUs
This commit contains work done by Eurecom and by Benetel.

openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_RA.c
@@ -435,7 +435,10 @@ void nr_add_msg3(module_id_t module_idP, int CC_id, frame_t frameP, sub_frame_t 
   NR_UE_list_t                               *UE_list = &mac->UE_list;
   int UE_id = 0;
 
-  AssertFatal(ra->state != RA_IDLE, "RA is not active for RA %X\n", ra->rnti);
+  if (ra->state != RA_IDLE) {
+    LOG_W(MAC,"RA is not active for RA %X. skipping msg3 scheduling\n", ra->rnti);
+    return;
+  }
 
   LOG_D(MAC, "[gNB %d][RAPROC] Frame %d, Subframe %d : CC_id %d RA is active, Msg3 in (%d,%d)\n", module_idP, frameP, slotP, CC_id, ra->Msg3_frame, ra->Msg3_slot);
 

cmake_targets/CMakeLists.txt
@@ -763,6 +763,42 @@ set(HWLIB_TCP_BRIDGE_OAI_SOURCE
 add_library(tcp_bridge_oai MODULE ${HWLIB_TCP_BRIDGE_OAI_SOURCE} )
 set_target_properties(tcp_bridge_oai PROPERTIES COMPILE_FLAGS "-fvisibility=hidden")
 
+# Benetel 4G library
+######################################################################
+
+set(HWLIB_BENETEL_4G_SOURCE
+  ${OPENAIR_TARGETS}/ARCH/ETHERNET/benetel/4g/benetel.c
+  ${OPENAIR_TARGETS}/ARCH/ETHERNET/benetel/4g/shared_buffers.c
+  ${OPENAIR_TARGETS}/ARCH/ETHERNET/benetel/4g/low.c
+  ${OPENAIR_TARGETS}/ARCH/ETHERNET/benetel/4g/low_dpdk.c
+  ${OPENAIR_TARGETS}/ARCH/ETHERNET/benetel/4g/dpdk_driver.c
+  )
+add_library(benetel_4g MODULE ${HWLIB_BENETEL_4G_SOURCE} )
+
+set_target_properties(benetel_4g PROPERTIES COMPILE_FLAGS "-fvisibility=hidden -march=native -I$ENV{RTE_SDK}/$ENV{RTE_TARGET}/include")
+
+SET(DPDK_LIBS "-Wl,-rpath,$ENV{RTE_SDK}/$ENV{RTE_TARGET}/lib -Wl,--whole-archive -L$ENV{RTE_SDK}/$ENV{RTE_TARGET}/lib -ldpdk -Wl,--no-whole-archive")
+TARGET_LINK_LIBRARIES(benetel_4g ${DPDK_LIBS})
+TARGET_LINK_LIBRARIES(benetel_4g pthread dl rt m numa)
+
+# Benetel 5G library
+######################################################################
+
+set(HWLIB_BENETEL_5G_SOURCE
+  ${OPENAIR_TARGETS}/ARCH/ETHERNET/benetel/5g/benetel.c
+  ${OPENAIR_TARGETS}/ARCH/ETHERNET/benetel/5g/shared_buffers.c
+  ${OPENAIR_TARGETS}/ARCH/ETHERNET/benetel/5g/low.c
+  ${OPENAIR_TARGETS}/ARCH/ETHERNET/benetel/5g/low_dpdk.c
+  ${OPENAIR_TARGETS}/ARCH/ETHERNET/benetel/5g/dpdk_driver.c
+  )
+add_library(benetel_5g MODULE ${HWLIB_BENETEL_5G_SOURCE} )
+
+set_target_properties(benetel_5g PROPERTIES COMPILE_FLAGS "-fvisibility=hidden -march=native -I$ENV{RTE_SDK}/$ENV{RTE_TARGET}/include")
+
+SET(DPDK_LIBS "-Wl,-rpath,$ENV{RTE_SDK}/$ENV{RTE_TARGET}/lib -Wl,--whole-archive -L$ENV{RTE_SDK}/$ENV{RTE_TARGET}/lib -ldpdk -Wl,--no-whole-archive")
+TARGET_LINK_LIBRARIES(benetel_5g ${DPDK_LIBS})
+TARGET_LINK_LIBRARIES(benetel_5g pthread dl rt m numa)
+
 ##########################################################
 
 include_directories ("${OPENAIR_TARGETS}/ARCH/COMMON")

executables/nr-ru.c
@@ -180,6 +180,17 @@ int attach_rru(RU_t *ru)
         ((RRU_config_t *)&rru_config_msg.msg[0])->prach_ConfigIndex[0]);
   AssertFatal((ru->ifdevice.trx_ctlsend_func(&ru->ifdevice,&rru_config_msg,rru_config_msg.len)!=-1),
               "RU %d failed send configuration to remote radio\n",ru->idx);
+
+  if ((len = ru->ifdevice.trx_ctlrecv_func(&ru->ifdevice,
+             &rru_config_msg,
+             msg_len))<0) {
+    LOG_I(PHY,"Waiting for RRU %d\n",ru->idx);
+  } else if (rru_config_msg.type == RRU_config_ok) {
+    LOG_I(PHY, "RRU_config_ok received\n");
+  } else {
+    LOG_E(PHY,"Received incorrect message %d from RRU %d\n",rru_config_msg.type,ru->idx);
+  }
+
   return 0;
 }
 
@@ -1442,6 +1453,8 @@ void *ru_thread( void *param ) {
 
   LOG_I(PHY,"Starting RU %d (%s,%s),\n",ru->idx,NB_functions[ru->function],NB_timing[ru->if_timing]);
 
+  memcpy((void*)&ru->config,(void*)&RC.gNB[0]->gNB_config,sizeof(ru->config));
+
   if(emulate_rf) {
     fill_rf_config(ru,ru->rf_config_file);
     nr_init_frame_parms(&ru->config, fp);
@@ -1463,8 +1476,6 @@ void *ru_thread( void *param ) {
 
       AssertFatal(ret==0,"Cannot connect to remote radio\n");
     }
-
-    memcpy((void*)&ru->config,(void*)&RC.gNB[0]->gNB_config,sizeof(ru->config));
 
     if (ru->if_south == LOCAL_RF) { // configure RF parameters only
       nr_init_frame_parms(&ru->config, fp);
@@ -1571,7 +1582,8 @@ void *ru_thread( void *param ) {
     if (slot_type == NR_UPLINK_SLOT || slot_type == NR_MIXED_SLOT) {
     //if (proc->tti_rx==8) {
 
-      if (ru->feprx) ru->feprx(ru,proc->tti_rx);
+      if (ru->feprx) {
+      ru->feprx(ru,proc->tti_rx);
       //LOG_M("rxdata.m","rxs",ru->common.rxdata[0],1228800,1,1);
 
       LOG_D(PHY,"RU proc: frame_rx = %d, tti_rx = %d\n", proc->frame_rx, proc->tti_rx);
@@ -1590,6 +1602,7 @@ void *ru_thread( void *param ) {
 		       ru->prach_list[prach_id].prachStartSymbol,
 		       proc->frame_rx,proc->tti_rx);
 	free_nr_ru_prach_entry(ru,prach_id);
+      }
       }
     }
 
@@ -2131,26 +2144,29 @@ void set_function_spec_param(RU_t *ru) {
         ru->fh_north_out         = NULL;                    // no outgoing fronthaul to north
         ru->nr_start_if          = NULL;                    // no if interface
         ru->rfdevice.host_type   = RAU_HOST;
+
+	// FK this here looks messed up. The following lines should be part of the  if (ru->function == gNodeB_3GPP), shouldn't they?
+
+	ru->fh_south_in            = rx_rf;                               // local synchronous RF RX
+	ru->fh_south_out           = tx_rf;                               // local synchronous RF TX
+	ru->start_rf               = start_rf;                            // need to start the local RF interface
+	ru->stop_rf                = stop_rf;
+	ru->start_write_thread     = start_write_thread;                  // starting RF TX in different thread
+	printf("configuring ru_id %u (start_rf %p)\n", ru->idx, start_rf);
       }
 
-      ru->fh_south_in            = rx_rf;                               // local synchronous RF RX
-      ru->fh_south_out           = tx_rf;                               // local synchronous RF TX
-      ru->start_rf               = start_rf;                            // need to start the local RF interface
-      ru->stop_rf                = stop_rf;
-      ru->start_write_thread     = start_write_thread;                  // starting RF TX in different thread
-      printf("configuring ru_id %u (start_rf %p)\n", ru->idx, start_rf);
       /*
-          if (ru->function == gNodeB_3GPP) { // configure RF parameters only for 3GPP eNodeB, we need to get them from RAU otherwise
-            fill_rf_config(ru,rf_config_file);
-            init_frame_parms(&ru->frame_parms,1);
-            nr_phy_init_RU(ru);
-          }
+	printf("configuring ru_id %u (start_rf %p)\n", ru->idx, start_rf);
+	fill_rf_config(ru,rf_config_file);
+	init_frame_parms(&ru->frame_parms,1);
+	nr_phy_init_RU(ru);
 
-          ret = openair0_device_load(&ru->rfdevice,&ru->openair0_cfg);
-          if (setup_RU_buffers(ru)!=0) {
-            printf("Exiting, cannot initialize RU Buffers\n");
-            exit(-1);
-          }*/
+	ret = openair0_device_load(&ru->rfdevice,&ru->openair0_cfg);
+	if (setup_RU_buffers(ru)!=0) {
+	   printf("Exiting, cannot initialize RU Buffers\n");
+	   exit(-1);
+	}
+      */
       break;
 
     case REMOTE_IF5: // the remote unit is IF5 RRU
@@ -2201,6 +2217,15 @@ void set_function_spec_param(RU_t *ru) {
       if (ret<0) {
         printf("Exiting, cannot initialize transport protocol\n");
         exit(-1);
+      }
+
+      if (ru->ifdevice.get_internal_parameter != NULL) {
+        void *t = ru->ifdevice.get_internal_parameter("fh_if4p5_south_in");
+        if (t != NULL)
+          ru->fh_south_in = t;
+        t = ru->ifdevice.get_internal_parameter("fh_if4p5_south_out");
+        if (t != NULL)
+          ru->fh_south_out = t;
       }
 
       malloc_IF4p5_buffer(ru);

openair1/SCHED_NR/fapi_nr_l1.c
@@ -219,7 +219,7 @@ void nr_schedule_response(NR_Sched_Rsp_t *Sched_INFO){
         LOG_D(PHY,"frame %d, slot %d, Got NFAPI_NR_UL_TTI_PRACH_PDU_TYPE for %d.%d\n", frame, slot, UL_tti_req->SFN, UL_tti_req->Slot);
         nfapi_nr_prach_pdu_t *prach_pdu = &UL_tti_req->pdus_list[i].prach_pdu;
         nr_fill_prach(gNB, UL_tti_req->SFN, UL_tti_req->Slot, prach_pdu);
-        nr_fill_prach_ru(gNB->RU_list[0], UL_tti_req->SFN, UL_tti_req->Slot, prach_pdu);
+        if (gNB->RU_list[0]->if_south == LOCAL_RF) nr_fill_prach_ru(gNB->RU_list[0], UL_tti_req->SFN, UL_tti_req->Slot, prach_pdu);
         break;
     }
   }

openair2/LAYER2/NR_MAC_COMMON/nr_mac_common.c
@@ -1151,8 +1151,9 @@ uint8_t compute_nr_root_seq(NR_RACH_ConfigCommon_t *rach_config,
     if (NCS == 0) return nb_preambles;
     else {
       r = L_ra/NCS;
-      printf(" found_sequences %u\n", (nb_preambles/r));
-      return (nb_preambles/r);
+      found_sequences = (nb_preambles/r) + (nb_preambles%r!=0); //ceil(nb_preambles/r)
+      printf(" found_sequences %u\n", found_sequences);
+      return (found_sequences);
     }
   }
   else{

openair2/LAYER2/NR_MAC_gNB/gNB_scheduler.c
@@ -451,6 +451,29 @@ void gNB_dlsch_ulsch_scheduler(module_id_t module_idP,
     else
       UE_list->fiveG_connected[UE_id] = true;
 
+    if (get_softmodem_params()->phy_test == 1) {
+      if (slot_txP == 7){
+	NR_RA_t *ra = &RC.nrmac[module_idP]->common_channels[0].ra[0];
+	ra->Msg2_frame = frame_txP;
+	ra->Msg2_slot = slot_txP;
+	ra->state = Msg2;
+	ra->bwp_id = 1;
+	NR_CellGroupConfig_t *secondaryCellGroup = UE_list->secondaryCellGroup[UE_id];
+	NR_BWP_Downlink_t *bwp=secondaryCellGroup->spCellConfig->spCellConfigDedicated->downlinkBWP_ToAddModList->list.array[ra->bwp_id-1];
+	struct NR_PDCCH_ConfigCommon__commonSearchSpaceList *commonSearchSpaceList = bwp->bwp_Common->pdcch_ConfigCommon->choice.setup->commonSearchSpaceList;
+	for (int i=0;i<commonSearchSpaceList->list.count;i++) {
+	  NR_SearchSpace_t *ss=commonSearchSpaceList->list.array[i];
+	  if(ss->searchSpaceId == *bwp->bwp_Common->pdcch_ConfigCommon->choice.setup->ra_SearchSpace)
+	    ra->ra_ss=ss;
+	}
+	AssertFatal(ra->ra_ss!=NULL,"no search space for RA'n");
+
+	nr_generate_Msg2(module_idP, 0/*CC_id*/,
+			 frame_txP,
+			 slot_txP);
+      }
+    }
+
     // Phytest scheduling
 
     if (get_softmodem_params()->phy_test) {

openair2/RRC/LTE/rrc_eNB.c
@@ -9075,46 +9075,52 @@ void rrc_eNB_process_AdditionResponseInformation(const module_id_t enb_mod_idP, 
     return;
   }
 
-  protocol_ctxt_t ctxt;
-  rrc_eNB_ue_context_t *ue_context;
-  unsigned char buffer[8192];
-  int size;
-  ue_context = rrc_eNB_get_ue_context(RC.rrc[enb_mod_idP], m->rnti);
-  ue_context->ue_context.nb_of_modify_endc_e_rabs = m->nb_e_rabs_admitted_tobeadded;
-  int j=0;
+    protocol_ctxt_t ctxt;
+    rrc_eNB_ue_context_t *ue_context;
+    unsigned char buffer[8192];
+    int size;
 
-  while(j < m->nb_e_rabs_admitted_tobeadded) {
-    for (int e_rab_idx=0; e_rab_idx<ue_context->ue_context.setup_e_rabs; e_rab_idx++) {
-      //Update ue_context information with gNB's address and new GTP tunnel ID
-      if( ue_context->ue_context.e_rab[e_rab_idx].param.e_rab_id == m->e_rabs_admitted_tobeadded[j].e_rab_id) {
-        memcpy(ue_context->ue_context.gnb_gtp_endc_addrs[e_rab_idx].buffer,
-               m->e_rabs_admitted_tobeadded[j].gnb_addr.buffer,
-               m->e_rabs_admitted_tobeadded[j].gnb_addr.length);
-        ue_context->ue_context.gnb_gtp_endc_addrs[e_rab_idx].length = m->e_rabs_admitted_tobeadded[j].gnb_addr.length;
-        ue_context->ue_context.gnb_gtp_endc_teid[e_rab_idx] = m->e_rabs_admitted_tobeadded[j].gtp_teid;
-        ue_context->ue_context.e_rab[e_rab_idx].status = E_RAB_STATUS_TOMODIFY;
-        break;
+    ue_context = rrc_eNB_get_ue_context(RC.rrc[enb_mod_idP], m->rnti);
+    if (ue_context) {
+      ue_context->ue_context.nb_of_modify_endc_e_rabs = m->nb_e_rabs_admitted_tobeadded;
+
+      int j=0;
+      while(j < m->nb_e_rabs_admitted_tobeadded){
+	for (int e_rab_idx=0; e_rab_idx<ue_context->ue_context.setup_e_rabs; e_rab_idx++){
+	  //Update ue_context information with gNB's address and new GTP tunnel ID
+	  if( ue_context->ue_context.e_rab[e_rab_idx].param.e_rab_id == m->e_rabs_admitted_tobeadded[j].e_rab_id){
+	    memcpy(ue_context->ue_context.gnb_gtp_endc_addrs[e_rab_idx].buffer,
+		   m->e_rabs_admitted_tobeadded[j].gnb_addr.buffer,
+		   m->e_rabs_admitted_tobeadded[j].gnb_addr.length);
+	    ue_context->ue_context.gnb_gtp_endc_addrs[e_rab_idx].length = m->e_rabs_admitted_tobeadded[j].gnb_addr.length;
+	    ue_context->ue_context.gnb_gtp_endc_teid[e_rab_idx] = m->e_rabs_admitted_tobeadded[j].gtp_teid;
+	    ue_context->ue_context.e_rab[e_rab_idx].status = E_RAB_STATUS_TOMODIFY;
+	    break;
+	  }
+	}
+	j++;
       }
+
+      PROTOCOL_CTXT_SET_BY_INSTANCE(&ctxt,
+                                    0,
+                                    ENB_FLAG_YES,
+                                    m->rnti,
+                                    0, 0);
+
+      size = rrc_eNB_generate_RRCConnectionReconfiguration_endc(&ctxt, ue_context, buffer, 8192, scg_CellGroupConfig, nr1_conf);
+
+      rrc_data_req(&ctxt,
+                   DCCH,
+                   rrc_eNB_mui++,
+                   SDU_CONFIRM_NO,
+                   size,
+                   buffer,
+                   PDCP_TRANSMISSION_MODE_CONTROL);
+    } else {
+      LOG_E(F1AP, "no ue_context for RNTI %x, acknowledging release\n", m->rnti);
     }
 
-    j++;
-  }
-
-  PROTOCOL_CTXT_SET_BY_INSTANCE(&ctxt,
-                                0,
-                                ENB_FLAG_YES,
-                                m->rnti,
-                                0, 0);
-  size = rrc_eNB_generate_RRCConnectionReconfiguration_endc(&ctxt, ue_context, buffer, 8192, scg_CellGroupConfig, nr1_conf);
-  rrc_data_req(&ctxt,
-               DCCH,
-               rrc_eNB_mui++,
-               SDU_CONFIRM_NO,
-               size,
-               buffer,
-               PDCP_TRANSMISSION_MODE_CONTROL);
 }
-
 
 //-----------------------------------------------------------------------------
 void *rrc_enb_process_itti_msg(void *notUsed) {

targets/ARCH/COMMON/common_lib.h
@@ -434,6 +434,11 @@ struct openair0_device_t {
    * \param arg pointer to capabilities or configuration
    */
   int (*trx_write_init)(openair0_device *device);
+  /* \brief Get internal parameter
+   * \param id parameter to get
+   * \return a pointer to the parameter
+   */
+  void *(*get_internal_parameter)(char *id);
 };
 
 /* type of device init function, implemented in shared lib */

commit 9215872cc47b528ef48962f4404d321237ba92ff

fixing a stupid mistake introduced in da3dd6b8

openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_RA.c
@@ -437,7 +437,7 @@ void nr_add_msg3(module_id_t module_idP, int CC_id, frame_t frameP, sub_frame_t 
   NR_UE_list_t                               *UE_list = &mac->UE_list;
   int UE_id = 0;
 
-  if (ra->state != RA_IDLE) {
+  if (ra->state == RA_IDLE) {
     LOG_W(MAC,"RA is not active for RA %X. skipping msg3 scheduling\n", ra->rnti);
     return;
   }