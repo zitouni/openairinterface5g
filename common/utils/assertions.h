bd2cc7cf90929b14b9d57b83c1d27af292d15e5a
Zitouni, Rafik Dr (Comp Sci & Elec Eng) <rz0005@surrey.ac.uk>
2024-07-16 12:35:13 +0100

    Add a folder CONF_ICS to gather the ICS conf files

--- common/utils/assertions.h
+++ common/utils/assertions.h
@@ -27,7 +27,8 @@
 #include <inttypes.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include "common/platform_types.h"
+//#include "common/platform_types.h"
+#include "../platform_types.h"
 
 #define OAI_EXIT_NORMAL 0
 #define OAI_EXIT_ASSERT 1
