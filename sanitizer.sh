e54302a761be95a351956e9d78b7b01a373e8049
Zitouni, Rafik Dr (Comp Sci & Elec Eng) <rz0005@surrey.ac.uk>
2024-09-13 13:50:48 +0100

    Fix of the warrnings E2 and logs of F1

--- sanitizer.sh
+++ sanitizer.sh
@@ -1,1 +1,1 @@
-sudo docker build --target ran-build --tag ran-build:latest --file docker/Dockerfile.build.ubuntu20 --build-arg "BUILD_OPTION=--sanitize" .
+sudo docker build --target ran-build --tag ran-build:latest --file docker/Dockerfile.build.ubuntu22 --build-arg "BUILD_OPTION=--sanitize" .
